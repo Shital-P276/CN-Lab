ccc30cb9e63a5b6186886fcd1a86ab5d439b6a2f
Sam-Wadmare <159400424+Sam-Wadmare@users.noreply.github.com>
2024-10-22 16:02:59 +0530

    Update stopandwait.c

--- stopandwait.c
+++ stopandwait.c
@@ -1,21 +1,34 @@
 #include<stdio.h>
+#include<unistd.h>
 
-void main(){
-    int i,num;
-    printf("\t\tStop and wait ARQ\n\n");
-    printf("Enter the frame number to be lost:");
+void stopnwait(int frame,int windowsize) {
+	int i;
+	printf("SENDER \t\t RECEIVER \n ");
+	for(i=1;i<=windowsize;i++) {
+		if (i == frame ) {
+			printf("frame %d sent \t frame %d not received \n",i,i);
+		}
+		else {
+			printf("frame %d sent \t frame %d received \n",i,i);
+		}
+	}
+	printf("Resending...\n");
+	sleep(1.5);
+	for (i=1;i<=windowsize;i++) {
+		sleep(1);
+		printf("frame %d sent \t frame %d received \n",i,i);
+	}
+}
 
-    scanf("%d",&num);
-    printf("\nSender \t\t\t\tReciever\n");
-    for(i=1;i<=5;i++){
-        
-        printf("\nFrame %d sent",i);
-        if(i==num){
-            printf("\t\tACK Frame %d not recieved. \nResending ....",i);
-            printf("\nFrame %d Sent\n\t\t\tACK Frame %d recieved\n",i,i);
-        }
-        else{
-            printf("\t\tACK Frame %d Recieved\n",i);
-        }
-    }
+int main() {
+	int windowsize,frame;
+	printf("enter windowsize :  ");
+	scanf("%d",&windowsize);
+	
+	printf("enter frame to be lost : ");
+	scanf("%d",&frame);
+	
+	stopnwait(frame,windowsize);
+	
+	return 0;
 }
